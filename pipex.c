83fbfd9150ced24820395ef172c73989156673ed
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-08-10 15:36:34 +0200

    statr to mess w/ pipes

--- pipex.c
+++ pipex.c
@@ -1,7 +1,11 @@
 #include "libft.h"
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#define STDIN 0
+#define STDOUT 1
 
 char	*ft_append(char *s1, char *s2)
 {
@@ -51,7 +55,7 @@ int	path_error(char **paths, int i)
 {
 	if (!paths[i])
 	{
-		perror(paths[i]);
+		perror("path error");
 		free_tab(paths);
 		return (1);
 	}
@@ -85,36 +89,83 @@ char	*get_path(char *program_name, char **envp)
 	return (path);
 }
 
-int	execute(char *path, char **new_av, char **envp)
+int	execute(char *path, char **new_av, char **envp, int i, int max, int fds[2], int pipes[2][2])
 {
 	pid_t	pid;
 
+	//close(pipes[0][0]);
+	//close(pipes[0][1]);
+	//close(pipes[1][0]);
+	//close(pipes[1][1]);
+	printf("i = %i\n", i);
+	if (i == 3)
+	{
+		dup2(fds[0], STDIN);
+		//dup2(fds[1], STDOUT);
+		dup2(pipes[0][1], STDOUT);
+		close(pipes[0][0]);
+		close(pipes[1][0]);
+		close(pipes[1][1]);
+	}
+	else if (i == max - 1)
+	{
+		dup2(pipes[i % 2][0], STDIN);
+		dup2(fds[1], STDOUT);
+		close(pipes[0][0]);
+		close(pipes[0][1]);
+		close(pipes[1][0]);
+		close(pipes[1][1]);
+	}
+	else if (i % 2 == 1)
+	{
+		dup2(pipes[1][1], pipes[0][0]);
+		dup2(pipes[0][1], STDOUT);
+		close(pipes[0][0]);
+		close(pipes[1][0]);
+		close(pipes[1][1]);
+	}
+	else if (i % 2 == 0)
+	{
+		dup2(pipes[0][1], STDIN);
+		dup2(pipes[1][1], STDOUT);
+		close(pipes[0][0]);
+		close(pipes[0][1]);
+		close(pipes[1][0]);
+	}
 	pid = fork();
 	if (pid == -1)
 		return (1);
-	else if (pid == 0)
+	if (pid == 0)
 		execve(path, new_av, envp);
-	wait(0);
+	wait(&pid);
 	return (0);
 }
 
 int	main(int ac, char **av, char **envp)
 {
 	int		i;
+	int		fds[2];
 	char	*path;
 	char	**new_av;
+	int		pipes[2][2];
 
-	i = 0;
+	i = 2;
+	fds[0] = open(av[1], O_RDWR);
+	fds[1] = open(av[ac - 1], O_RDWR);
+	if (pipe(pipes[0]) == -1 || pipe(pipes[1]) == -1)
+		return (3);
 	while (++i < ac)
 	{
-		new_av = ft_split(av[i], ' ');
+		printf("cmd %i\n", i-2);
+		new_av = ft_split(av[i - 1], ' ');
 		if (!new_av || !*new_av || !**new_av) //is that ok ?
 			return (free_all_error(new_av, path, 1));
 		path = get_path(new_av[0], envp);
 		if (!path || !*path)
 			return (free_all_error(new_av, path, 2));
-		execute(path, new_av, envp);
+		execute(path, new_av, envp, i, ac, fds, pipes);
 		free_all(new_av, path);
 	}
+	printf("End of pipex\n");
 	return (0);
 }
